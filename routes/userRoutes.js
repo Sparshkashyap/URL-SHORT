90be70332787b0ca8b75313393e966cf988080d3
Sparsh Kashyap <sparsh@example.com>
2025-03-30 00:54:43 +0530

    url-shortner🚀

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 const {ShortURL,redirecturl,Analytics,Render} = require('../controllers/userControllers');
-const { RestrictUserOnlyTO } = require('../middleware/userMiddleware');
+// const { RestrictUserOnlyTO } = require('../middleware/userMiddleware');
 const {signpage} = require('../controllers/alluser');
 const {admin} = require('../controllers/alluser')
 
@@ -14,8 +14,8 @@ const {admin} = require('../controllers/alluser')
     router.get('/signup',signpage);
     router.get('/:sid',redirecturl);
     router.get('/visit/:sid',Analytics);
-    router.get('/admin/url',RestrictUserOnlyTO(["ADMIN"]),admin);
-    router.get('/', RestrictUserOnlyTO(["NORMAL","ADMIN"]),Render); // Staticlogin (homepage)
+    router.get('/admin/url',admin);
+    router.get('/',Render); // Staticlogin (homepage)
  
     
 
