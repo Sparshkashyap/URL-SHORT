4a132eb9e4a4c17bf9ade4b2e37d68aa1f5e05a0
Sparsh Kashyap <sparsh@example.com>
2025-03-30 00:41:05 +0530

    url-shortner🚀

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 const connectdatabase = async () => {
 
-    mongoose.connect(process.env.MY_URL)
-    // mongoose.connect('mongodb+srv://sparshkashyap655:FTE2Qcap4jOKLHEu@my-cluster.u0brbyk.mongodb.net/')
+    // mongoose.connect(process.env.MY_URL)
+    mongoose.connect('mongodb+srv://sparshkashyap655:1234@my-cluster.u0brbyk.mongodb.net/?retryWrites=true&w=majority&appName=my-cluster')
     .then((user)=>console.log("Mongodb connected...."))
     .catch((err)=>console.log("Error occur :",err));
     
